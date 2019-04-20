277595f318cf084caa2bff9182d3d6c1b3b8b56a
Paul Roberts <pmr@stelo.org.uk>
2019-04-20 21:49:41 +0100

    reimplement part1 in a way that will make part2 easier

--- day07/advent07.rb
+++ day07/advent07.rb
@@ -1,30 +1,32 @@
 require 'set'
 
 class Advent
-  def initialize
+  def initialize(deps)
     f = proc do | h,k | h[k] = Set.new end
     @h   = Hash.new &f
     @inv = Hash.new &f
+    @state = Hash.new
+    deps.each do |k,v|
+      add_dep k, v
+    end
   end
 
   def add_dep(k, v)
     @h[k].add v
-    @h[v]
+    @state[k] = :init
+    @state[v] = :init
     @inv[v].add k
   end
 
   def resolve(k)
-    # puts "resolving #{k}"
-    @inv[k].each do |j|
-      # puts "\tremoving from #{j}"
-      @h[j].delete k
-    end
-    @h.delete k
+    @state[k] = :complete
   end
 
   def ready
-    @h.select do |k,v|
-      v.length == 0
+    @state.select do |k,v|
+      v == :init and @h[k].all? do |j|
+        @state[j] == :complete
+      end
     end.map do |k, v|
       k
     end.sort
@@ -32,17 +34,19 @@ class Advent
 
 end
 
-a = Advent.new
-
 fname = ARGV.length > 0 ? ARGV[0] : "example.txt"
 input = File.readlines fname
+
+@deps = []
 
 input.map do |l|
   if not %r(Step ([A-Z]) must be finished before step ([A-Z]) can begin.).match(l)
     puts "Bad line"
   end
-  a.add_dep $2, $1
+  @deps.push [$2, $1]
 end
+
+a = Advent.new @deps
 
 order = []
 while a.ready.length > 0
@@ -52,3 +56,9 @@ while a.ready.length > 0
 end
 
 puts order.join
+
+#def part2
+  #a = Advent.new @deps
+#end
+
+#puts part1
