c51e09c1d3665850415c12c8cc7e32ac8d7127e2
Paul Roberts <pmr@stelo.org.uk>
2019-05-14 23:24:25 +0100

    ruby integral image

--- day11/advent11.rb
+++ day11/advent11.rb
@@ -1,10 +1,10 @@
 class Advent
-  attr_reader :serial, :grid
+  attr_reader :serial, :max_x, :max_y
   def initialize(serial, max_x = 300, max_y = 300)
     @serial = serial
     @max_x = max_x
     @max_y = max_y
-    @memo  = {}
+    @image = {}
   end
 
   def power_of(x, y)
@@ -13,53 +13,66 @@ class Advent
     power.to_s[-3].to_i - 5
   end
 
-  def power_of_sq(x, y, sz)
-    @memo[[x,y,sz]] ||=
+  def coords(lim = 0)
+    (1..max_y-lim-1).flat_map do |y|
+      (1..max_x-lim-1).map { |x| [x,y] }
+    end
+  end
+
+  def image(x,y)
+    @image[[x,y]] ||=
       begin
-        puts [x,y,sz].join(",")
-        if sz == 1 then power_of(x,y)
+        if x < 1 or y < 1 or x > max_x or y > max_y
+          0
         else
-          row = (x..x-1+sz).map do |x|
-            # puts "#{x},#{y-1+sz} = #{power_of(x,y-1+sz)}"
-            power_of(x,y-1+sz)
-          end.sum
-          col = (y..y-2+sz).map do |y|
-            # puts "#{x-1+sz},#{y} = #{power_of(x-1+sz,y)}"
-            power_of(x-1+sz,y)
-          end.sum
-          # puts "======"
-          row + col + power_of_sq(x,y,sz-1)
+          power_of(x,y) + image(x - 1, y) + image(x, y - 1) - image(x - 1, y - 1)
         end
       end
   end
 
-  def solve1(sz=3)
-    (1..@max_y-sz-1).flat_map do |y|
-      (1..@max_x-sz-1).map do |x|
-        [ x, y, power_of_sq(x,y,sz) ]
-      end
-    end.max_by { |a| a[2] }
+  def square(x,y,sz)
+    a = image(x-1,y-1)
+    b = image(x-1,y-1+sz)
+    c = image(x-1+sz,y-1)
+    d = image(x-1+sz,y-1+sz)
+    (d - b - c) + a
   end
 
-  def solve_for(x,y)
-    lim = [ @max_x + 1 - x, @max_y + 1 - y ].min
-    # puts "x: #{x}, y: #{y}, lim: #{lim}"
-    sz = (1..lim).max_by { |sz| power_of_sq(x,y,sz) }
-    [ x, y, sz, power_of_sq(x,y,sz) ]
+  def part1
+    coords(3).max_by do |x,y|
+      square(x,y,3)
+    end
   end
 
-  def solve2
-    (1..@max_y).flat_map do |y|
-      (1..@max_x).map do |x|
-        solve_for(x,y)
+  def part2
+    max = nil
+
+    (1..max_x).each do |sz|
+      coords(sz).each do |x,y|
+        r = [x,y,sz,square(x,y,sz)]
+        # p r
+        if not max
+          max = r
+        else
+          max = max[3] < r[3] ? r : max
+        end
       end
-    end.max_by { |a| a[3] }
+    end
+    max
   end
 end
 
-#puts Advent.new(8199).solve1
+# advent = Advent.new(8199)
+advent = Advent.new(18)
+# puts advent.part1.join(",")
+puts advent.part2.join(",")
+
+# puts Advent.new(18).square(90,269,16)
+
+# puts Advent.new(18).image(1,1)
+
 #p Advent.new(18).solve_for(90,269)
-p Advent.new(18).solve2
+# p Advent.new(18).solve2
 # p Advent.new(42).solve_for(232,251)
 # p Advent.new(18).power_of_sq(33,45,3)
 # puts Advent.new(8199).solve2
