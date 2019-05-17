3f43f51745fb484a321884e854602b0ccb4ade96
Paul Roberts <pmr@stelo.org.uk>
2019-05-17 20:42:47 +0100

    set

--- day12/advent12.rb
+++ day12/advent12.rb
@@ -1,26 +1,67 @@
+require 'set'
+
 class State
   def initialize
-    @pots_left  = Array.new
-    @pots_right = Array.new
+    @plants = Set.new
   end
 
   def has_plant?(pot)
-    (pot < 0 ? @pots_left[pot.abs] : @pots_right[pot]) == true
+    @plants.member? pot
   end
 
   def has_plant(pot)
-    if(pot < 0)
-      @pots_left[pot.abs] = true
+    @plants.add pot
+  end
+
+  def seg(pot)
+    (pot-2..pot+2).map { |p| if has_plant?(p) then "#" else "." end }.join
+  end
+
+  def count
+    @plants.sum
+  end
+
+  def self.from_s(s)
+    res = State.new
+    s.chars.each_with_index do |c,i|
+      res.has_plant(i) if c == "#"
+    end
+    res
+  end
+
+  def bounds
+    ( @plants.min-2..@plants.max+2 )
+  end
+
+  def next(rules, count = 1)
+    if count == 0
+      self
     else
-      @pots_right[pot] = true
+      bounds.each_with_object(State.new) { | pot, state |
+        # puts "#{pot} #{seg(pot)} #{rules[seg(pot)]}"
+        state.has_plant(pot) if rules[seg(pot)]
+      }.next(rules, count-1)
     end
   end
 
   def to_s
-    (@pots_left.reverse + @pots_right).map { |p| p ? "#" : "." }.join()
+    bounds.map { |pot|
+      has_plant?(pot) ? "#" : "."
+    }.join
   end
 end
 
-s = State.new
-s.has_plant(100)
-puts s
+def read_file(f)
+  lines = File.readlines(f)
+  /initial state: (.+)$/.match(lines.first)
+  s = State.from_s($1)
+  rules = lines.drop(2).map do |l|
+    [ l.slice(0, 5), l.slice(9) == "#" ]
+  end.to_h
+  [ s, rules ]
+end
+
+# s, rules = read_file "example.txt"
+s, rules = read_file "input12.txt"
+puts "Part 1: #{s.next(rules, 20).count}"
+puts "Part 2: #{s.next(rules, 50000000000).count}"
